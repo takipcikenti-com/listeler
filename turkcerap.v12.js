700086a580536885b7bff05ea4c948755f04b135
Yılmaz <55016557+yenisarkilarlistesi@users.noreply.github.com>
2020-01-26 21:08:22 +0300

    Update and rename turkcerap.v11.js to turkcerap.v12.js

--- turkcerap.v12.js
+++ turkcerap.v12.js
@@ -2,6 +2,9 @@
 AP.init({
   playList: [
 {'title': 'Şanışer - Susamam (Uzun Versiyon)', 'file': 'https://firebasestorage.googleapis.com/v0/b/gitbook-28427.appspot.com/o/assets%2F-LN6pmp-hlaXw_W0OEcN%2F-LoRQpd0IsdKaIvKwtjR%2F-LoRRTq7xTHQgIy_dgTz%2Fsusamam.mp3?alt=media&token=2c25fe72-f75b-4bf2-ac80-6e952b804ef1'},
+{'title': 'Murda & Ezhel - Bi Sonraki Hayatımda Gel', 'file': 'https://firebasestorage.googleapis.com/v0/b/gitbook-28427.appspot.com/o/assets%2F-Lpdr52D2axjjm7pA8Aa%2F-LzNcAGkAR96Xtfzu8HE%2F-LzNeEmjVQTaXidkAR1x%2Fbisonrakihayatimdagel.mp3?alt=media&token=26fe8e1d-5107-4a2d-ba69-9e39ec3c46e4'},
+{'title': 'Velet - İki Lira', 'file': 'https://firebasestorage.googleapis.com/v0/b/gitbook-28427.appspot.com/o/assets%2F-Lpdr52D2axjjm7pA8Aa%2F-LzY0RVj46N4D6q13kMr%2F-LzY10eQRpC3ZCcri1GC%2Fikilira.mp3?alt=media&token=56cb08b0-c098-4a42-a197-f0bed87c478e'},
+{'title': 'Ceg & Anıl Piyancı - Yeşillendir', 'file': 'https://firebasestorage.googleapis.com/v0/b/gitbook-28427.appspot.com/o/assets%2F-Lpdr52D2axjjm7pA8Aa%2F-LzY0RVj46N4D6q13kMr%2F-LzY0YvF6ARCccqkOSIA%2Fyesillendir.mp3?alt=media&token=131e3ff0-fa9a-4848-9fa0-c12721926972'},
 {'title': 'Ceza - Beatcoin', 'file': 'https://firebasestorage.googleapis.com/v0/b/gitbook-28427.appspot.com/o/assets%2F-Lpdr52D2axjjm7pA8Aa%2F-Lx70Agxe5v1nnP6DTJ0%2F-Lx74Qlv3wmS_VHPHUgL%2Fbeatcoin.mp3?alt=media&token=9a6785ca-f741-430c-9f7c-d80904bd8c65'},
 {'title': 'Rozz Kalliope & Ece Seçkin - Benjamins 3', 'file': 'https://firebasestorage.googleapis.com/v0/b/gitbook-28427.appspot.com/o/assets%2F-Lpdr52D2axjjm7pA8Aa%2F-LvkaEHt3sYZvY7I88A8%2F-Lvkbz-GG4UwJcCAVxIS%2Fbenjamins3.mp3?alt=media&token=024e70cc-d859-42ce-8c37-4934d39e7bfb'},
 {'title': 'Sagopa Kajmer - Toz Taneleri', 'file': 'https://firebasestorage.googleapis.com/v0/b/gitbook-28427.appspot.com/o/assets%2F-Lpdr52D2axjjm7pA8Aa%2F-LvkdnymlP35QT-dqJ7U%2F-LvkfDIFF2Qro6iCpJp3%2Ftoztaneleri.mp3?alt=media&token=b70cbc96-8e29-4b44-b2b9-12befa8b6402'},
@@ -9,7 +12,6 @@ AP.init({
 {'title': 'Taladro ft. Cem Adrian - Unutmak İstemiyorum', 'file': 'https://firebasestorage.googleapis.com/v0/b/gitbook-28427.appspot.com/o/assets%2F-Lpdr52D2axjjm7pA8Aa%2F-LwjfOAFDDkxcH9D2ILg%2F-LwjgoRzWTO6Zlkrwi0I%2Funutmakistemiyorum.mp3?alt=media&token=f21a34b8-82ee-4974-a256-87cf95957e7f'},
 {'title': 'Velet - Leyla', 'file': 'https://firebasestorage.googleapis.com/v0/b/gitbook-28427.appspot.com/o/assets%2F-Lpdr52D2axjjm7pA8Aa%2F-Ly5gop6LLjGK9cb_t5F%2F-Ly5hDK-ob6UwNP1ldeW%2Fleyla.mp3?alt=media&token=db44b30d-cac4-4a31-bade-83d86298a74a'},
 {'title': 'Sezzy - Beni Bul', 'file': 'https://firebasestorage.googleapis.com/v0/b/gitbook-28427.appspot.com/o/assets%2F-Lpdr52D2axjjm7pA8Aa%2F-Lyp136GA6n8j_I5K_Xj%2F-Lyp16E4YqIkjtcLcQkY%2Fbenibul.mp3?alt=media&token=689ddc80-1b58-425e-8f35-3cba7856fb89'},
-    
 {'title': 'KADR - Zalim Dünya', 'file': 'https://firebasestorage.googleapis.com/v0/b/gitbook-28427.appspot.com/o/assets%2F-Lpdr52D2axjjm7pA8Aa%2F-Lyp6isa2gbVlVlDnYJ5%2F-Lyp6mZY9gxnvBkxfJ57%2Fzalimdunya.mp3?alt=media&token=0da66f59-380e-4851-b0e9-3e1df48287ba'},
 {'title': 'Stap - Memur', 'file': 'https://firebasestorage.googleapis.com/v0/b/gitbook-28427.appspot.com/o/assets%2F-Lpdr52D2axjjm7pA8Aa%2F-Ly5gop6LLjGK9cb_t5F%2F-Ly5i5H9cLESPafSXovZ%2Fmemur.mp3?alt=media&token=29e594eb-dd23-4b6d-bf59-8e2bcfe962d5'},
 {'title': 'Afi Ares - Anka', 'file': 'https://firebasestorage.googleapis.com/v0/b/gitbook-28427.appspot.com/o/assets%2F-Lpdr52D2axjjm7pA8Aa%2F-Ly5dyKB0MbvWZfSpbyk%2F-Ly5empOCZyUIhUKdjiD%2Fanka.mp3?alt=media&token=80d48dfb-601c-4102-89f6-383934d990f3'},
